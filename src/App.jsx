2022bd625303d59dfb44814c7ba2f36ced2c60dd
Hariprasath-2218 <aileaner2218@gmail.com>
2025-07-03 12:23:25 +0530

    adding loadingScreen componenet

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
 import { Toaster } from "@/components/ui/toaster"
+import { LoadingScreen } from "./components/LoadingScreen"
+import { useState } from "react"
 
 function App() {
-
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
       <Toaster/>
       <BrowserRouter>
         <Routes>
